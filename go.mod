2c2add5bc69666ff80e542437ffe827b0da71ac2
Maas Lalani <maaslalani1@gmail.com>
2020-07-08 15:06:51 -0400

    basic cli

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/maaslalani/sentry
 
 go 1.14
+
+require (
+	github.com/urfave/cli v1.22.4
+	github.com/urfave/cli/v2 v2.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	app := &cli.App{
+		Name:  "Sentry",
+		Usage: "Run commands on file changes",
+		Action: func(c *cli.Context) error {
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
