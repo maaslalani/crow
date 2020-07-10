8378e571c8a28a1d3de34256703dfb31e0dcdd08
Maas Lalani <maaslalani1@gmail.com>
2020-07-10 15:09:25 -0400

    add another test

--- main_test.go
+++ main_test.go
@@ -9,27 +9,66 @@ import (
 	"time"
 )
 
-func TestMain(t *testing.T) {
-	pwd, _ := os.Getwd()
-	app := App(pwd)
+func TestCommand(t *testing.T) {
+	t.Run("command execution", func(t *testing.T) {
+		pwd, _ := os.Getwd()
+		app := App(pwd)
 
-	tf, teardown := MockOs(t)
-	defer teardown()
+		tf, teardown := MockOs(t)
+		defer teardown()
 
-	time.AfterFunc(50*time.Millisecond, func() {
-		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		time.AfterFunc(100*time.Millisecond, func() {
+			syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		})
+
+		err := app.RunContext(nil, []string{"crow", "echo", "foo"})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fc, err := ioutil.ReadFile(tf.Name())
+
+		if !strings.Contains(string(fc), "foo") {
+			t.Fatal("command did not run correctly")
+		}
 	})
+}
 
-	err := app.RunContext(nil, []string{"crow", "echo", "hello"})
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestChanges(t *testing.T) {
+	t.Run("file changes", func(t *testing.T) {
+		pwd, _ := os.Getwd()
+		app := App(pwd)
 
-	fc, err := ioutil.ReadFile(tf.Name())
+		tf, teardown := MockOs(t)
+		defer teardown()
 
-	if !strings.Contains(string(fc), "hello") {
-		t.Fatal("command not run correctly")
-	}
+		f, err := os.Create("foo.text")
+		defer f.Close()
+		defer os.Remove(f.Name())
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		time.AfterFunc(100*time.Millisecond, func() {
+			syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		})
+
+		err = app.RunContext(nil, []string{"crow", "echo", "bar"})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = f.WriteString("change")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fc, err := ioutil.ReadFile(tf.Name())
+		if !strings.Contains(string(fc), "bar") {
+			t.Fatal("command did not run multiple times")
+		}
+	})
 }
 
 func MockOs(t *testing.T) (*os.File, func()) {
